68943d33b5d2a279523d91c3fb5dde5e3a17c6e1
1138-4EB <1138-4EB@users.noreply.github.com>
2019-02-26 17:53:26 +0100

    use net/http.DetectContentType to identify non-text files

--- files/utils.go
+++ files/utils.go
@@ -1,12 +1,9 @@
 package files
 
+import (
+	"net/http"
+)
+
 func isBinary(content string) bool {
-	for _, b := range content {
-		// 65533 is the unknown char
-		// 8 and below are control chars (e.g. backspace, null, eof, etc)
-		if b <= 8 || b == 65533 {
-			return true
-		}
-	}
-	return false
+	return http.DetectContentType([]byte(content))[0:4] != "text"
 }
